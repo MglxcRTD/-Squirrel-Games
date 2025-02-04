ceef952a5157bf4346195c3baaf2a0e882cffdf1
Miguel <miguelroadtodam@gmail.com>
2025-02-04 18:44:35 +0100

    Primer commit con las clases de los personajes

---
+++ src/pinkguards/SupervisorException.java
@@ -0,0 +1,9 @@
+package pinkguards;
+
+class SupervisorException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public SupervisorException(String message) {
+		super(message);
+	}
+}
