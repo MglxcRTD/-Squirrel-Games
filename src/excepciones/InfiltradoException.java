9d18abec602f6235e17b9102a72fc4e351c4f299
Miguel <miguelroadtodam@gmail.com>
2025-02-11 16:04:29 +0100

    Primer commit + actualizacion de implementacion de excepcion infiltrado

---
+++ src/excepciones/InfiltradoException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class InfiltradoException extends Exception {
+	private static final long serialVersionUID = 2L;
+
+	public InfiltradoException(String message) {
+		super(message);
+	}
+}
