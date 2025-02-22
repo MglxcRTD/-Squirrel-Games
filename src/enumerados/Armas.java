7d80ba57b5e3a9bbdd0f7643ea44092610a4fbec
Crisveling <crisvelrc@gmail.com>
2025-02-22 01:05:02 +0100

    Correccion de nombre de armas actualizado correctamente.

--- src/enumerados/Armas.java
+++ src/enumerados/Armas.java
@@ -1,7 +1,7 @@
 package enumerados;
 
 public enum Armas {
-	PISTOLA_GLOCK17("Pistola Glock177"), RIFLE_ASALTO("Rifle de Asalto"), HECKLER_KOCHMP5("Heckler Koch MP5"),
+	PISTOLA_GLOCK17("Pistola Glock17"), RIFLE_ASALTO("Rifle de Asalto"), HECKLER_KOCHMP5("Heckler Koch MP5"),
 	RIFLE_AUTOMATICO("Rifle Automatico"), HECKLER_KOCHG3("Heckler Koch G3"),
 	SMITH_WESSON_MODEL10("Smith & Wesson Model 10");
 
